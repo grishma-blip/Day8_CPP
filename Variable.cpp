77e2c3d37b55253ea40c2ca171018f383ac12a9c
Grishma Thakare <85503530+grishma-blip@users.noreply.github.com>
2024-12-08 17:13:11 +0530

    Add files via upload

---
+++ Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int var1 = 3;
+    int var2 = 24;
+    int var3 = 17;
+    cout << "address of var1:"<<&var1<<endl;
+    cout << "address of var2:"<<&var2<<endl;
+    cout << "address of var3:"<<&var3<<endl;
+}
